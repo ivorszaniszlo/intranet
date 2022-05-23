69d17691d81c9dc5a0909ff2c9abc3256fb7a7cc
Szaniszló Ivor <szaniszlo.ivor@ringieraxelspringer.hu>
2022-05-23 15:07:18 +0200

    ADD dynamic menus to header and footer

--- app/public/wp-content/themes/intranet/functions.php
+++ app/public/wp-content/themes/intranet/functions.php
@@ -12,6 +12,9 @@ add_action('wp_enqueue_scripts', 'intranet_files');
 
 //add browser tag every page
 function intranet_features() {
+    register_nav_menu('headerMenuLocation', 'Header Menu Location');
+    register_nav_menu('footerLocationOne', 'Footer Location One');
+    register_nav_menu('footerLocationTwo', 'Footer Location Two');
     add_theme_support('title-tag');
 }
 
