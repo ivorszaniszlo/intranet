72696e16f6663e0aea2284158c8502969ea3679f
Szaniszló Ivor <szaniszlo.ivor@ringieraxelspringer.hu>
2022-05-26 15:35:11 +0200

    ADD: displaying custom Post Types (Events)

--- app/public/wp-content/mu-plugins/intranet-post-types.php
+++ app/public/wp-content/mu-plugins/intranet-post-types.php
@@ -2,16 +2,20 @@
 
 function intranet_post_types() {
     register_post_type('event', array(
-        'public' => true ,
+        'rewrite' => array('slug' => 'events'),
+        'has_archive' => true,
+        'public' => true,
+        'show_in_rest' => true,
         'labels' => array(
-            'name' => 'Events',
-            'add_new_item' => 'Add NEw Event',
-            'edit_item' => 'Edit Event',
-            'all_items' => 'All Events',
-            'singular_name' => 'Event'
+          'name' => 'Events',
+          'add_new_item' => 'Add New Event',
+          'edit_item' => 'Edit Event',
+          'all_items' => 'All Events',
+          'singular_name' => 'Event'
         ),
-        'menu_icon' => 'dashicons-calendar' 
-    ));
-}
+        'menu_icon' => 'dashicons-calendar'
+      ));
+    }
+    
 
 add_action('init', 'intranet_post_types');
